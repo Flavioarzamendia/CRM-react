9e6a561e675b0e5a3d797215ab0cfd26a29a83fc
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2023-12-29 17:39:45 -0300

    accion de agregar y eliminar cliente

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -1,29 +1,57 @@
-const Clientes = ({cliente,key}) => {
-    const {nombre, empresa, email, telefono , id} = cliente
-  return (
-    <tr className="border-b">
-      <td className="p-6">
-        <p className=" text-2xl text-gray-800 space-y-2">{nombre}</p>
-        <p>{empresa}</p>
-      </td>
+import { useNavigate, Form, redirect } from 'react-router-dom'
+import { eliminarCliente } from '../data/Clientes'
 
-      <td className="p-6">
-       <p className=" text-gray-600">
-        <span className="text-gray-800 uppercase font-bold">Email: </span>{email}
-       </p>
-       <p className=" text-gray-600">
-        <span className="text-gray-800 uppercase font-bold">Tel: </span>{telefono}
-       </p>
-      </td>
+export async function action({params}) {
+    await eliminarCliente(params.clienteId)
+    return redirect('/')
+}
 
-      <td className="p-6 flex gap-3">
-        <button type="button" className="px-8 py-2 text-blue-700 uppercase font-bold text-xs">Editar</button>
-        <button type="button" className="px-6 py-2 text-red-500 uppercase font-bold text-xs">Eliminar</button>
+function Clientes({cliente}) {
 
-      </td>
-      
-    </tr>
-  );
-};
+    const navigate = useNavigate()
+    const { nombre, empresa, email, telefono, id} = cliente
 
-export default Clientes;
+    return (
+        <tr className="border-b">
+            <td className='p-6 space-y-2'>
+                <p className="text-2xl text-gray-800">{nombre}</p>
+                <p>{empresa}</p>
+            </td>
+
+            <td className="p-6">
+                <p className="text-gray-600"> <span className="text-gray-800 uppercase font-bold">Email: </span>{email} </p>
+                <p className="text-gray-600"> <span className="text-gray-800 uppercase font-bold">Tel: </span>{telefono} </p>
+            </td>
+
+            <td className="p-6 flex gap-3 justify-end">
+                <button
+                    type="button"
+                    className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+                    onClick={() => navigate(`/clientes/${id}/editar`) }
+                >
+                    Editar
+                </button>
+
+            <Form
+                method='post'
+                action={`/clientes/${id}/eliminar`}
+                onSubmit={(e) => {
+                    if(!confirm('¿Deseas eliminar este registro?')) {
+                        e.preventDefault()
+                    }
+                }}
+            >
+                <button
+                    type="submit"
+                    className="text-red-600 hover:text-red-700 uppercase font-bold text-xs "
+                >
+                    Eliminar
+                </button>
+            </Form>
+  
+            </td>
+        </tr>
+    )
+}
+
+export default Clientes
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import NuevoCliente,{action as nuevoClienteAction} from "./pages/NuevoCliente";
 import Index,{loader as clientesLoader} from "./pages/Index";
+import EditarCliente,{loader as editarClienteLoader, action as actualizarClienteAction} from "./pages/EditarCliente";
 import ErrorPage from "./components/ErrorPage";
+import {action as eliminarClienteAction} from "./components/Clientes";
 
 const router = createBrowserRouter([
   {
@@ -18,11 +20,26 @@ const router = createBrowserRouter([
         loader: clientesLoader,
         errorElement: <ErrorPage/>
       },
+      
       {
         path: "/clientes/nuevos",
         element: <NuevoCliente/>,
         action: nuevoClienteAction,
+        errorElement: <ErrorPage/>
       },
+      {
+        path:'/clientes/:clienteId/editar',
+        element: <EditarCliente/>,
+        loader : editarClienteLoader,
+        action : actualizarClienteAction,
+        errorElement: <ErrorPage/>
+      },
+      {
+        path:'/clientes/:clienteId/eliminar',
+        action: eliminarClienteAction
+
+      }
+      
     ],
   },
 ]);
